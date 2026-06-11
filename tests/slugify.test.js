cb1b0fa5952337a3aa42be1d998e7d6e3574541e
Ayhan güveli <ayhanguveli@gmail.com>
2026-06-11 09:44:38 +0300

    feat: add slugify helper with tests

---
+++ tests/slugify.test.js
@@ -0,0 +1,19 @@
+const { test } = require("node:test");
+const assert = require("node:assert");
+const { slugify } = require("../lib/slugify.js");
+
+test("Türkçe karakterleri çevirir ve boşlukları tireler", () => {
+  assert.strictEqual(
+    slugify("Kalkan'da Deniz Manzaralı Müstakil Parsel"),
+    "kalkan-da-deniz-manzarali-mustakil-parsel"
+  );
+});
+
+test("baş/son tireleri temizler", () => {
+  assert.strictEqual(slugify("  --Satılık!! "), "satilik");
+});
+
+test("boş girişte 'ilan' döner", () => {
+  assert.strictEqual(slugify(""), "ilan");
+  assert.strictEqual(slugify("!!!"), "ilan");
+});
